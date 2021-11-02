d4b642e750d5eb561bbefb478ae0dfdfbd8c9651
olga <qa.olga.lina@gmail.com>
2021-11-02 15:46:56 -0700

    Added tests for user register

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -1,9 +1,42 @@
+import pytest
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
 
 
 class TestUserRegister(BaseCase):
+    invalid_registration_data = ([
+        {
+            "username": "learnqa",
+            "firstName": "learnqa",
+            "lastName": "learnqa",
+            "email": "learnqa@example.com"
+        },
+        {
+            "password": "123",
+            "firstName": "learnqa",
+            "lastName": "learnqa",
+            "email": "learnqa@example.com"
+        },
+        {
+            "password": "123",
+            "username": "learnqa",
+            "lastName": "learnqa",
+            "email": "learnqa@example.com"
+        },
+        {
+            "password": "123",
+            "username": "learnqa",
+            "firstName": "learnqa",
+            "email": "learnqa@example.com"
+        },
+        {
+            "password": "123",
+            "username": "learnqa",
+            "firstName": "learnqa",
+            "lastName": "learnqa",
+        }
+    ])
 
     def test_create_user_successfully(self):
         data = self.prepare_registration_data()
@@ -21,4 +54,42 @@ class TestUserRegister(BaseCase):
 
         Assertions.assert_code_status(response, 400)
         assert response.content.decode("utf-8") == f"Users with email '{email}' already exists", \
+            f"Unexpected response content {response.content}"
+
+    def test_create_user_with_incorrect_email(self):
+        email = "vinkotovexample.com"
+        data = self.prepare_registration_data(email)
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "Invalid email format", \
+            f"Unexpected response content {response.content}"
+
+    @pytest.mark.parametrize("data", invalid_registration_data)
+    def test_create_user_with_missing_fields(self, data):
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert "The following required params are missed:" in response.content.decode("utf-8"), \
+            f"Unexpected response content {response.content}"
+
+    def test_create_user_with_short_username(self):
+        data = self.prepare_registration_data()
+        data.update({"username": "a"})
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "The value of 'username' field is too short", \
+            f"Unexpected response content {response.content}"
+
+    def test_create_user_with_long_username(self):
+        data = self.prepare_registration_data()
+        data.update({"username": str("a" * 251)})
+
+        response = MyRequests.post("/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
+        assert response.content.decode("utf-8") == "The value of 'username' field is too long", \
             f"Unexpected response content {response.content}"
