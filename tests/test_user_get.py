8972081ce3c253444e59d6cf1f73de7e09183182
olga <qa.olga.lina@gmail.com>
2021-11-02 18:08:40 -0700

    Added tests for get user details

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -5,6 +5,12 @@ from lib.assertions import Assertions
 
 class TestUserGet(BaseCase):
 
+    def create_new_user(self):
+        user_data = self.prepare_registration_data()
+        response = MyRequests.post("/user/", data=user_data)
+        Assertions.assert_code_status(response, 200), "New user not created"
+        return response, user_data
+
     def test_get_user_details_not_auth(self):
         response = MyRequests.get("/user/2")
         Assertions.assert_json_has_key(response,"username")
@@ -32,3 +38,33 @@ class TestUserGet(BaseCase):
 
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    def test_get_user_details_auth_as_different_user(self):
+        # Create first user and get user id
+        response, user_data = self.create_new_user()
+        first_user_id = response.json()["id"]
+
+        # Create second user and get data needed for authorization
+        response, user_data = self.create_new_user()
+        second_user_email = user_data["email"]
+        second_user_password = user_data["password"]
+
+        # Authorize second user
+        data = {
+            "email": second_user_email,
+            "password": second_user_password
+        }
+        response1 = MyRequests.post("/user/login", data=data)
+        auth_sid = self.get_cookie(response1, "auth_sid")
+        token = self.get_header(response1, "x-csrf-token")
+
+        # Get first user's details authorized as second user
+        response2 = MyRequests.get(
+            f"/user/{first_user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+        Assertions.assert_json_has_key(response2, "username")
+        Assertions.assert_json_has_not_key(response2, "email")
+        Assertions.assert_json_has_not_key(response2, "firstName")
+        Assertions.assert_json_has_not_key(response2, "lastName")
